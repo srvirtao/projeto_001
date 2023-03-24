9697ec6dda5b7608032847f7b90b42b75b285f06
srvirtao <68133396+srvirtao@users.noreply.github.com>
2023-03-23 23:13:08 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function verificarAniversario() {
+    var anoNascimento = document.getElementById("ano-nascimento").value;
+    var diaNascimento = document.getElementById("dia-nascimento").value;
+
+    // cria um objeto Date com a data atual
+    var hoje = new Date();
+
+    // verifica se o dia e o mês de hoje são iguais ao dia e mês de nascimento informados
+    if (hoje.getDate() == diaNascimento && hoje.getMonth() + 1 == 12) {
+        // se for o aniversário do usuário, exibe a mensagem de parabéns
+        document.getElementById("resultado").innerHTML = "Parabéns pelo seu dia, meu amor! Você é a melhor pessoa do mundo e eu te amo muito!";
+    } else {
+        // caso contrário, exibe uma mensagem informando que não é o aniversário do usuário
+        document.getElementById("resultado").innerHTML = "Hoje não é seu aniversário, mas não se preocupe, sempre te amo! <3";
+    }
+}
+
+// adiciona um listener ao botão "Verificar" para chamar a função verificarAniversario() quando ele for clicado
+document.getElementById("btn-verificar").addEventListener("click", verificarAniversario);
